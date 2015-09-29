dc5286009126fd906f2522569232c4bc366c78b2
locobunney <lauritaa@gilmour.org>
2015-09-29 16:22:31 -0400

    game code is finished but not totally bug free

--- src/Ideone.java
+++ src/Ideone.java
@@ -9,7 +9,7 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		boolean battle = true;
+		boolean battle = true; int score = 0;
 		System.out.println("Pick a hero, player");
 		Scanner scanner = new Scanner(System.in);
 	    String heroname = scanner.nextLine();
@@ -19,13 +19,44 @@ class Ideone
 	    Champion lol = new Champion(heroname,yo);
 	    while (battle = true){
 	    	Monster evil = new Monster((int)(Math.random()*4)+1);
-	    	toBattle();
-	    }
+	    	toBattle(heroname, evil, lol, yo);
+	    	if (lol.getHealth()<=0){
+	    		System.out.print("game over, score: "+score);
+	    		break;
+	    	}
+	    	score++;
+	    } 
 	     
 		// ask the user to create a weapon and then create a champion. Extra credit if you build a battle function
 	}
-	public static void toBattle(){
-		
+	public static void toBattle(String heroname, Monster evil, Champion lol, Weapon yo){
+		System.out.println(heroname+" has encountered a "+ evil.getName());
+		int dmgdealt; int choice;
+		Scanner scanner = new Scanner(System.in);
+		while ((evil.getHealth()>0) && (lol.getHealth()>0)){
+			System.out.println("Press 1 to attack and 2 to heal");
+			choice = scanner.nextInt();
+			if (choice==1){
+				dmgdealt = yo.getDmg() + (int)(Math.random()*5)+2;
+				System.out.println(evil.getName()+" was stuck by "+ yo.getName());
+				evil.takeDmg(dmgdealt);
+				System.out.println(dmgdealt+" damage was dealt to the "+evil.getName());
+			}
+			else if (choice==2){
+				dmgdealt = ((int)(Math.random()*5)+12)*-1;
+				System.out.println(lol.getName()+" drinks a potion and restores "+-dmgdealt);
+				lol.takeDmg(dmgdealt);
+			}
+			System.out.println("the "+evil.getName()+" attacks");
+			dmgdealt=evil.getDmg()+ (int)(Math.random()*3)+1;
+			lol.takeDmg(dmgdealt);
+			System.out.println(dmgdealt+" damage dealt to "+lol.getName());
+			System.out.println(lol.getName()+" has "+lol.getHealth()+" health remaining");
+			System.out.println("the "+evil.getName()+" has "+evil.getHealth()+" health remaining");
+			if (evil.getHealth()<0){
+				System.out.println("the "+evil.getName()+" was slain");
+			}
+		}
 	}
 }
 class Monster
@@ -36,19 +67,23 @@ class Monster
 	
 	public Monster(int m)
 	{
-		switch (m){
+		switch(m){
 			case 1: name = "Wolf";
 					health = 40;
 					dmg = 15;
+					break;
 			case 2: name = "Goblin";
 					health = 50;
 					dmg = 13;
+					break;
 			case 3: name = "Bandit";
 					health = 80;
 					dmg = 11;
+					break;
 			case 4: name = "Troll";
 					health = 150;
 					dmg = 5;
+					break;
 		}
 	}
 	public int getHealth(){
@@ -56,6 +91,12 @@ class Monster
 	}
 	public void takeDmg(int dmgdealt){
 		health -= dmgdealt;
+	}
+	public String getName(){
+		return name;
+	}
+	public int getDmg(){
+		return dmg;
 	}
 }
 
@@ -81,6 +122,9 @@ class Champion  //if this class was in a separate file it would need to be calle
 	 public void takeDmg(int dmgdealt){
 		 health -= dmgdealt;
 	 }
+	 public String getName(){
+			return name;
+	}
 }
 
 class Weapon
@@ -94,7 +138,12 @@ class Weapon
 		name = wpnname;
 		dmg = (int)(Math.random()*5)+10;
 	}
-
+	public int getDmg(){
+		return dmg;
+	}
+	public String getName(){
+		return name;
+	}
 	//create get functions a SET is not required
 
 }
---
+++ src/testing.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class testing {
+	public static void main(String[]args){
+		{
+			
+		}
+	}
+}
